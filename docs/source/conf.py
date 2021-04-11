21332a62cb2e40b27d933cc1aa826a48a98aaf78
niccoloalexander <nahamlin@gmail.com>
2021-04-11 18:02:09 +0800

    Added content to docs using recommonmark markdown parser.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,13 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from recommonmark.parser import CommonMarkParser
 
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
 
 # -- Project information -----------------------------------------------------
 
@@ -30,7 +36,7 @@ release = '0.2.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
